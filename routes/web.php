94747da6f5edf41ffc4a5a79fcb687e3e00e9f64
Chhounseangly <Chhounseangly74@gmail.com>
2026-01-30 17:19:23 +0700

    update some code

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\LeaveTemplateController;
+use App\Http\Controllers\LeaveTemplateWorkingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [LeaveTemplateController::class, 'index'])->name('leave_template.index');
-Route::get('/templates/create', [LeaveTemplateController::class, 'create'])->name('leave_template.create');
-Route::get('/templates/edit/{id}', [LeaveTemplateController::class, 'edit'])->name('leave_template.create');
-Route::post('/templates/store', [LeaveTemplateController::class, 'store'])->name('leave_template.store');
-Route::put('/templates/update/{id}', [LeaveTemplateController::class, 'update'])->name('leave_template.update');
-Route::get('/templates/generate-pdf/{id}', [LeaveTemplateController::class,'generatePdf'])->name('leave_template.generate.pdf');
+Route::get('/', [LeaveTemplateWorkingController::class, 'index'])->name('leave_template.index');
+Route::get('/templates/create', [LeaveTemplateWorkingController::class, 'create'])->name('leave_template.create');
+Route::get('/templates/edit/{id}', [LeaveTemplateWorkingController::class, 'edit'])->name('leave_template.create');
+Route::post('/templates/store', [LeaveTemplateWorkingController::class, 'store'])->name('leave_template.store');
+Route::put('/templates/update/{id}', [LeaveTemplateWorkingController::class, 'update'])->name('leave_template.update');
+Route::get('/templates/generate-pdf/{id}', [LeaveTemplateWorkingController::class,'generatePdf'])->name('leave_template.generate.pdf');
